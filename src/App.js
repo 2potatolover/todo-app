b916a9a093b343f8fac340ef9a7be181c25d392b
2potatolover <90419238+2potatolover@users.noreply.github.com>
2024-10-19 14:44:46 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,129 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import './border.css'
-<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+import {AiOutlineDelete, AiOutlineEdit} from 'react-icons/ai';
 
-function App() {
+function App () {
+  const [allTodos, setTodos] = useState ([]);
+  const [newTitle, setNewTitle] = useState ('');
+  const [currentEdit,setCurrentEdit] = useState("");
+  const [currentEditedItem,setCurrentEditedItem] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+    };
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push (newTodoItem);
+    setTodos (updatedTodoArr);
+    localStorage.setItem ('todolist', JSON.stringify (updatedTodoArr));
+  };
+
+  const handleDeleteTodo = index => {
+    let reducedTodo = [...allTodos];
+    reducedTodo.splice (index);
+
+    localStorage.setItem ('todolist', JSON.stringify (reducedTodo));
+    setTodos (reducedTodo);
+  };
+
+
+
+  const handleEdit = (ind,item)=>{
+    console.log(ind);
+    setCurrentEdit(ind);
+    setCurrentEditedItem(item);
+  }
+
+  const handleUpdateTitle = (value)=>{
+    setCurrentEditedItem((prev)=>{
+      return {...prev,title:value}
+    })
+  }
+
+
+  const handleUpdateToDo = ()=>{
+      let newToDo = [...allTodos];
+      newToDo[currentEdit] = currentEditedItem;
+      setTodos(newToDo);
+      setCurrentEdit("");
+  }
+
+
+
   return (
+    <div className="App">
+      <h1>Juan's Todo list (edit)</h1>
 
-  <html>
-  <body>
-<h1> TO DO LIST </h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={e => setNewTitle (e.target.value)}
+              placeholder="What's the task title?"
+            />
+          </div>
+          <div className="todo-input-item">
+          </div>
+          <div className="todo-input-item">
+            <button
+              type="button"
+              onClick={handleAddTodo}
+              className="primaryBtn"
+            >
+              Add
+            </button>
+          </div>
+        </div>
 
-<div class="w3-bar w3-red">
-  <div class="w3-bar-item">Add a todo to get started!</div>
+        <div className="todo-list">
 
-</div>
-</body>
-</html>
+          {allTodos.map ((item, index) => {
+              if(currentEdit===index){
+                 return(
+                  <div className='edit__wrapper' key={index}>
+                  <input placeholder='Updated Title' 
+                  onChange={(e)=>handleUpdateTitle(e.target.value)} 
+                  value={currentEditedItem.title}  /> 
+                   <button
+              type="button"
+              onClick={handleUpdateToDo}
+              className="primaryBtn"
+            >
+              Update
+            </button>
+              </div> 
+                 ) 
+              }else{
+                return (
+                  <div className="todo-list-item" key={index}>
+                    <div>
+                      <h3>{item.title}</h3>
+                    </div>
+  
+                    <div>
+                      <AiOutlineDelete
+                        className="icon"
+                        onClick={() => handleDeleteTodo (index)}
+                        title="Delete?"
+                      />  
+                      <AiOutlineEdit  className="check-icon"
+                        onClick={() => handleEdit (index,item)}
+                        title="Edit?" />
+                    </div>
+  
+                  </div>
+                );
+              }
+              
+            })}
 
+        </div>
+      </div>
+    </div>
   );
 }
 
